d53384225dd809f61b845b13b7038d67cc553e01
Andrew Berezovskyi <andriib@kth.se>
2023-07-17 02:10:10 +0200

    fix: force push with lease
    prevents losing commits
    
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- publisher.zig
+++ publisher.zig
@@ -323,7 +323,7 @@ fn commitAndPushRepoChange(log_repo_path: []const u8) !void {
 }
 
 fn pushRepoChange(log_repo_path: []const u8) !void {
-    try runNoCapture(log_repo_path, &[_][]const u8 {"git", "push", "origin", "HEAD:live", "-f"});
+    try runNoCapture(log_repo_path, &[_][]const u8 {"git", "push", "origin", "HEAD:live", "--force-with-lease"});
     // don't push to GH more frequently than once in 15 minutes
     std.time.sleep(@as(u64, 15 * 60) * std.time.ns_per_s); 
 }
